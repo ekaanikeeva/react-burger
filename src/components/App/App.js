3fd3a20e503d084b902b99262aac8f60e8d2ba87
ekaanikeeva <ekaanikeeva@yandex.ru>
2023-02-02 13:24:51 +0300

    refactor: clean up default files, move App, add modules

---
+++ src/components/App/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import styles from './App.module.css'
+import AppHeader from '../AppHeader/AppHeader'
+
+function App() {
+  return (
+    <div className={styles.root}>
+
+    </div>
+  );
+}
+
+export default App;
