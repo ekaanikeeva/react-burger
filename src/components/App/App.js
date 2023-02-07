69d2cf057a41cb16880fdacf8e16786fcaee2831
ekaanikeeva <ekaanikeeva@yandex.ru>
2023-02-07 16:12:14 +0300

    refactor: change hardcode data array for the real

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,14 +1,29 @@
-import React from 'react';
+import { useEffect, useState } from 'react';
 import styles from './App.module.scss'
-import { ingredientList } from '../../utils/data';
 import AppHeader from '../AppHeader/AppHeader'
 import Main from '../Main/Main';
+import { ingredientsUrl } from '../../utils/constants';
 
 function App() {
+  const [ingredients, setIngredients] = useState([]);
+
+  const getIngredients = () => {
+    fetch(ingredientsUrl)
+      .then(res => res.json())
+      .then(data => {
+        setIngredients(data.data)
+      })
+      .catch(err => console.log(err))
+  }
+
+  useEffect(() => {
+    getIngredients();
+  }, [])
+
   return (
     <div className={styles.root}>
-        <AppHeader />
-        <Main ingredients={ ingredientList } />
+      <AppHeader />
+      <Main ingredients={ingredients} />
     </div>
   );
 }
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,4 @@
+export const ingredientsUrl = 'https://norma.nomoreparties.space/api/ingredients';
 export const one = 'one';
 export const two = 'two';
 export const three = 'three';
