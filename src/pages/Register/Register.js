c7b84a6ea0bae0008dccfee686bed524c4d2e2f2
ekaanikeeva <ekaanikeeva@yandex.ru>
2023-04-07 12:38:30 +0300

    feat: register validation, refactor code

--- src/pages/Register/Register.js
+++ src/pages/Register/Register.js
@@ -5,66 +5,78 @@ import { useNavigate } from 'react-router-dom';
 import styles from './Register.module.scss';
 import Form from '../../components/Form/Form';
 import { registerUserAsync } from '../../services/asyncActions/auth';
+import { useValidation } from '../../utils/Validate';
 
 function Register() {
     const dispatch = useDispatch();
-
-    const [email, setEmail] = useState(null)
-    const [password, setPassword] = useState(null)
-    const [name, setName] = useState(null)
-
-    function handleChangeEmail(evt) {
-        const value = evt.target.value;
-        setEmail(value)
-    }
-
-    function handleChangeName(evt) {
-        const value = evt.target.value;
-        setName(value)
-    }
-
-    function handleChangePassword(evt) {
-        const value = evt.target.value;
-        setPassword(value)
-    }
+    const { values, handleChange, errors, isValid } = useValidation();
 
     function handleSubmit(e) {
         e.preventDefault()
 
-        dispatch(registerUserAsync(email, password, name))
+        dispatch(registerUserAsync(values.email, values.password, values.name))
     }
 
 
     return (
-        <Form 
-        title="Регистрация" submitTitle="Зарегистрироваться" onSubmit={handleSubmit}
-        link="/login" linkQuestion="Уже зарегистрированы?" linkText="Войти"
+        <Form
+            title="Регистрация" submitTitle="Зарегистрироваться" onSubmit={handleSubmit}
+            link="/login" linkQuestion="Уже зарегистрированы?" linkText="Войти"
         >
             <input
                 type="text"
                 name="name"
                 className={styles.input}
                 placeholder="Имя"
-                onChange={handleChangeName}
+                onChange={handleChange}
+                required
             />
+            <span
+                className={
+                    errors["name"] === ""
+                        ? styles.error
+                        : styles.error_active
+                }
+            >
+                {errors["name"]}
+            </span>
             <input
                 type="email"
                 name="email"
                 className={styles.input}
                 placeholder="E-mail"
-                // minLength={2}
-                // maxLength={50}
-                // pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$"
-                onChange={handleChangeEmail}
-                // required
+                minLength={2}
+                maxLength={50}
+                pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$"
+                onChange={handleChange}
+                required
             />
+            <span
+                className={
+                    errors["email"] === ""
+                        ? styles.error
+                        : styles.error_active
+                }
+            >
+                {errors["email"]}
+            </span>
             <input
                 type="password"
                 name="password"
                 className={styles.input}
                 placeholder="Пароль"
-                onChange={handleChangePassword}
+                onChange={handleChange}
+                required
             />
+            <span
+                className={
+                    errors["password"] === ""
+                        ? styles.error
+                        : styles.error_active
+                }
+            >
+                {errors["password"]}
+            </span>
         </Form>
     )
 }
