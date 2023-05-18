36c08c333e4c64e783af78c457477fbd97e2f4c1
ekaanikeeva <ekaanikeeva@yandex.ru>
2023-05-18 08:28:17 +0300

    refactor: AppHeader move from js to ts

---
+++ src/components/AppHeader/AppHeader.tsx
@@ -0,0 +1,35 @@
+import React, { FC } from "react";
+import styles from './AppHeader.module.scss'
+import { Logo, BurgerIcon, ProfileIcon, ListIcon } from '@ya.praktikum/react-developer-burger-ui-components'
+import { Link, NavLink } from "react-router-dom";
+
+const AppHeader:FC = () => {
+    return (
+        <header className={styles.header}>
+            <nav className={styles.navigation}>
+                <NavLink to='/' className={({ isActive }) =>
+                    isActive ? styles.activeLink : styles.link
+                }>
+                    <BurgerIcon type="primary" />
+                    Конструктор
+                </NavLink>
+                <a className={styles.link}>
+                    <ListIcon type="primary" />
+                    Лента заказов</a>
+            </nav>
+
+            <Link to='/' className={styles.logo}>
+                <Logo />
+            </Link>
+
+            <NavLink to='/profile' className={({ isActive }) =>
+                    isActive ? styles.activeLink : styles.account
+                }>
+                <ProfileIcon type="secondary" />
+                Личный кабинет
+            </NavLink>
+        </header>
+    );
+}
+
+export default AppHeader;
