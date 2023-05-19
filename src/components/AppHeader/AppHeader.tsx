f3246f4541131852b9c7f8b21745df3d75ef764e
ekaanikeeva <ekaanikeeva@yandex.ru>
2023-05-19 16:31:53 +0300

    chore: change type FC to FunctionComponent

--- src/components/AppHeader/AppHeader.tsx
+++ src/components/AppHeader/AppHeader.tsx
@@ -1,9 +1,9 @@
-import React, { FC } from "react";
+import { FunctionComponent } from "react";
 import styles from './AppHeader.module.scss'
 import { Logo, BurgerIcon, ProfileIcon, ListIcon } from '@ya.praktikum/react-developer-burger-ui-components'
 import { Link, NavLink } from "react-router-dom";
 
-const AppHeader:FC = () => {
+const AppHeader:FunctionComponent = () => {
     return (
         <header className={styles.header}>
             <nav className={styles.navigation}>
