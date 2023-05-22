756bfaed1f20bc16aa143ca4c2da82139b183ac2
ekaanikeeva <ekaanikeeva@yandex.ru>
2023-05-22 14:54:23 +0300

    refactor: rewrite Preloader to ts

--- src/components/Preloader/Preloader.tsx
+++ src/components/Preloader/Preloader.tsx
@@ -1,6 +1,7 @@
 import React from "react";
-import styles from "./Preloader.module.scss"
-function Preloader() {
+import styles from "./Preloader.module.scss";
+import { FunctionComponent } from 'react'
+const Preloader:FunctionComponent = () => {
     return (
         <div className={styles.preloader}>
             <span className="text text_type_digits-large">Loading...</span>
