cbcc5b50f3b98140549be759009775f8848e0653
ekaanikeeva <ekaanikeeva@yandex.ru>
2023-06-02 15:51:57 +0300

    refactor: typify ingredients async action

--- src/services/asyncActions/ingredients.ts
+++ src/services/asyncActions/ingredients.ts
@@ -1,11 +1,12 @@
 import { addIngredientsAction, getIngredientsSuccessAction, getIngredientsErrorAction } from "../actions/ingredientsActions";
 import { getIngredientsApi } from "../../utils/ingredientsApi";
+import { IIngredient, TAppDispatch } from "../../utils/tsUtils";
 
 export const ingredientsAsync = () => {
-    return function (dispatch) {
+    return function (dispatch:TAppDispatch) {
         getIngredientsApi()
         .then(res => {
-            const ingredientsList = res.data.map((item) => {
+            const ingredientsList = res.data.map((item:IIngredient) => {
                 item.count = 0;
                 return item;
             })
