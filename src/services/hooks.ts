4504f4fa97550f3abaab666f589debe1d1747bad
ekaanikeeva <ekaanikeeva@yandex.ru>
2023-06-20 15:46:23 +0300

    Updates

--- src/services/hooks.ts
+++
@@ -1,6 +0,0 @@
-import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
-import { TAppDispatch } from '../utils/tsUtils';
-    import type { RootState } from './store';
-
-    export const useAppDispatch = () => useDispatch<TAppDispatch>();
-    export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
